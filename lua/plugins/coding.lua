48738a75607572243eec215dcc8add19a6db1f3f
Yeji Han <yejicse@gmail.com>
2024-03-13 16:44:43 +0900

    Updated

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,13 @@
+return {
+  -- add Coqtail
+  { "whonore/Coqtail" },
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+}
