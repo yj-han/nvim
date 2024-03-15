1675efd894f3c0d673e7bff3ca8cbfc65deb7395
Yeji Han <yejicse@gmail.com>
2024-03-15 14:33:16 +0900

    Updated themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,7 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
   },
 }
