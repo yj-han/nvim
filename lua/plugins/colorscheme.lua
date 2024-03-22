3460d0270a1693b1c8526f5fe42ea853099fa344
Yeji Han <yejicse@gmail.com>
2024-03-22 14:06:06 +0900

    Updated coding, colorscheme packages

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,22 @@
 return {
+  -- Set the colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+
+  -- Add nordic
+  {
+    "AlexvZyl/nordic.nvim",
+    name = "nordic",
+    lazy = true,
+  },
+
   {
     "folke/tokyonight.nvim",
+    enable = false,
     lazy = true,
-    opts = { style = "moon" },
   },
 }
